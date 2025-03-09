d5f0c7d2097c029efa3e1a3c46c418c6d1eaa59e
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-03-09 21:35:28 +0900

    WIP: implementation of test code of game_repository.py

--- tests/test_game_repository.py
+++ tests/test_game_repository.py
@@ -40,39 +40,51 @@ def test_get_or_create_venue(setup_database):
 
 def test_save_game(setup_database):
     """試合情報の保存・更新"""
-    # save
+    venue1 = get_or_create_venue('Yankee Stadium')
+    venue2 = get_or_create_venue('Fenway Park')
     game_id = 12345
-    date = datetime(2025, 3,7)
-    venue = get_or_create_venue('Yankee Stadium')
-    save_game(game_id, date, venue.id)
+    date1 = datetime(2024, 6, 1)
+    date2 = datetime(2024, 6, 2)
+
+    # save
+    save_game(game_id, date1, venue1.id)
     # get
     game = get_game_by_id(game_id)
-    # test
+
     assert game is not None
     assert game.id == game_id
-    assert game.date == date
-    assert game.venue_id == venue.id
+    assert game.date == date1
+    assert game.venue_id == venue1.id
 
     # update
-    game_id = 12346
-    date = datetime(2025, 3,8)
+    save_game(game_id, date2, venue2.id) # 日付と球場を更新
+    updated_game = get_game_by_id(game_id)
+
+    assert updated_game is not None
+    assert updated_game.id == game_id
+    assert updated_game.date == date2 # 日付が更新されているか
+    assert updated_game.venue_id == venue2.id # 球場が更新されているか
+
+
+"""試合ごとのチーム情報を保存するテスト"""
+def test_save_game_team(setup_database):
+    # save new team
+    team = Team(id=1, name='New York Yankees')
+    db.session.add(team)
+    db.session.commit()
+
+    # save new game
+    game_id = 12345
+    date = datetime(2024, 6, 1)
     venue = get_or_create_venue('Yankee Stadium')
     save_game(game_id, date, venue.id)
-    # get
-    game = get_game_by_id(game_id)
-    # test
-    assert game is not None
-    assert game.id == game_id
-    assert game.date == date
-    assert game.venue_id == venue.id
 
+    # save team info every game
+    save_game_team(game_id, team.id, is_home_team=True)
 
+    game_team = GameTeam.query.filter_by(team_id=team.id, game_id=game_id).first()
 
-
-
-
-
-
-
-
-
+    assert game_team is not None
+    assert game_team.game_id == game_id
+    assert game_team.team_id == team.id
+    assert game_team.is_home_team is True
